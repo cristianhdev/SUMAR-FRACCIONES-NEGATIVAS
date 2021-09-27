31d92da54e125d15f35587423eea9ab237d2ea8a
cristianh <crishernandez08@hotmail.com>
2021-09-27 15:04:41 -0500

    subiendo funcionalidad hasta el ejercicio3

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 
 
-let sliders = ['slide-1', 'slide-2', 'slide-3']
-let audios_sond = ['Audio1', '', 'Audio2']
-let respuestasJuegoDemo=[5,3,7,4,4,1,4]
+let sliders = ['slide-1', 'slide-2', 'slide-3','slide-4']
+let audios_sond = ['Audio1', '', '']
+//Arrays Para configurar los numeros.
+let respuestasJuegoDemo = [5, 3, 7, 4, 4, 1, 4]
+let respuestasJuegoActividad1 = [
+	{numerador:[6,3,5,-2,18,10,8]},
+	{denominador:[5,3,15]},
+	{operacion1:[18,-10]},
+	{operacion2:[15]},
+	{resultado:[8,15]}
+]
+let respuestasJuegoActividad2 = [8, -15, 6, 12, 12, -1, 12]
 
 let presentacion_slide = 0
 let audio;
@@ -77,6 +86,7 @@ function cargarAudio(loop = false) {
 }
 
 function changeSound(new_sond) {
+	if (audios_sond[presentacion_slide] != null || audios_sond[presentacion_slide] != undefined) {
 	audio.src = `${path_sound}${new_sond}.mp3`;
 	audio.pause();
 	audio.load();
@@ -84,6 +94,7 @@ function changeSound(new_sond) {
 	audio.addEventListener('ended', function () {
 
 	});
+}
 }
 
 
@@ -93,19 +104,16 @@ function presentacion() {
 		document.getElementById('atras').style.visibility = "hidden"
 		//document.getElementById('visubility').style.display = "inline-block"
 
-	} else if (presentacion_slide == sliders.length - 2) {
+	} /* else if (presentacion_slide == sliders.length - 3) {
 		document.getElementById('siguiente').style.display = "none"
 		//document.getElementById('atras').style.display = "none"
 		document.getElementById('atras').style.visibility = "hidden"
 		document.getElementById('siguiente').style.display = "none"
-		changeSound(audios_sond[presentacion_slide])
-	} else if (presentacion_slide == sliders.length - 1) {
+	/* 	changeSound(audios_sond[presentacion_slide]) */
+	//} 
+	else {
 		document.getElementById('siguiente').style.display = "none"
-		//document.getElementById('atras').style.display = "none"
-		document.getElementById('atras').style.visibility = "hidden"
-		document.getElementById('siguiente').style.display = "none"
-	} else {
-		document.getElementById('siguiente').style.display = "inline-block"
+		document.getElementById('atras').style.display = "none"
 		document.getElementById('atras').style.visibility = "visible"
 		document.getElementById('actividad').style.display = "none"
 
@@ -160,10 +168,18 @@ function ReiniciarVariablesActividad() {
 			break;
 
 		case 2:
-			document.getElementById('resultado-2-actividad').classList.remove('imagen-correcta')
-			document.getElementById('resultado-2-actividad').classList.remove('imagen-incorrecta')
-			document.getElementById('resultado-1-actividad').classList.remove('imagen-correcta')
-			document.getElementById('resultado-1-actividad').classList.remove('imagen-incorrecta')
+			document.getElementById('resultado-3-actividad1').classList.remove('imagen-correcta')
+			document.getElementById('resultado-3-actividad1').classList.remove('imagen-incorrecta')
+			document.getElementById('resultado-2-actividad1').classList.remove('imagen-correcta')
+			document.getElementById('resultado-2-actividad1').classList.remove('imagen-incorrecta')
+			document.getElementById('resultado-1-actividad1').classList.remove('imagen-correcta')
+			document.getElementById('resultado-1-actividad1').classList.remove('imagen-incorrecta')
+			break;
+		case 3:
+			document.getElementById('resultado-2-actividad2').classList.remove('imagen-correcta')
+			document.getElementById('resultado-2-actividad2').classList.remove('imagen-incorrecta')
+			document.getElementById('resultado-1-actividad2').classList.remove('imagen-correcta')
+			document.getElementById('resultado-1-actividad2').classList.remove('imagen-incorrecta')
 			break;
 
 		default:
@@ -182,10 +198,10 @@ function loadArrayInputsDemo() {
 	let input4Demo = document.getElementById('demo4').value//Resultado1-3
 	let input5Demo = document.getElementById('demo5').value//Resultado2-4
 	let input6Demo = document.getElementById('demo6').value//InputFormula4-5
-	return [input1Demo, input2Demo, input3Demo, input4Demo, input5Demo,input6Demo]
+	return [input1Demo, input2Demo, input3Demo, input4Demo, input5Demo, input6Demo]
 }
 
-function loadArrayInputsActividad() {
+function loadArrayInputsActividad1() {
 	let input1 = document.getElementById('input1').value
 	let input2 = document.getElementById('input2').value
 	let input3 = document.getElementById('input3').value
@@ -212,6 +228,17 @@ function loadArrayInputsActividad() {
 	]
 }
 
+function loadArrayInputsActividad2() {
+
+	let input1Actividad2 = document.getElementById('InputB1').value//InputFormula1-0
+	let input2Actividad2 = document.getElementById('InputB2').value//InputFormula2-1
+	let input3Actividad2 = document.getElementById('InputB3').value//InputFormula3-2
+	let input4Actividad2 = document.getElementById('InputB4').value//Resultado1-3
+	let input5Actividad2 = document.getElementById('InputB5').value//Resultado2-4
+	let input6Actividad2 = document.getElementById('InputB6').value//InputFormula4-5
+	return [input1Actividad2, input2Actividad2, input3Actividad2, input4Actividad2, input5Actividad2, input6Actividad2]
+}
+
 function mostrarMensajeActividad() {
 	switch (getCurrentSlider()) {
 		case 1:
@@ -222,6 +249,10 @@ function mostrarMensajeActividad() {
 		case 2:
 			document.getElementById('mensajeActividad').style.visibility = 'visible'
 			document.getElementById('mensajeActividad').style.color = 'red'
+			break;
+		case 3:
+			document.getElementById('mensajeActividad2').style.visibility = 'visible'
+			document.getElementById('mensajeActividad2').style.color = 'red'
 			break;
 
 		default:
@@ -255,15 +286,19 @@ function comprobar() {
 	switch (getCurrentSlider()) {
 		case 1:
 			ReiniciarVariablesActividad()
-
-
 			arrayvalidador = loadArrayInputsDemo()
+			Actividad1(arrayvalidador);
 			break;
 
 		case 2:
 			ReiniciarVariablesActividad()
-
-			arrayvalidador = loadArrayInputsActividad()
+			arrayvalidador = loadArrayInputsActividad1()
+			Actividad2(arrayvalidador);
+			break;
+		case 3:
+			ReiniciarVariablesActividad()
+			arrayvalidador = loadArrayInputsActividad2()
+			Actividad3(arrayvalidador);
 			break;
 
 		default:
@@ -273,7 +308,12 @@ function comprobar() {
 
 
 
-	let resultadofind = arrayvalidador.includes("")
+
+}
+
+
+function Actividad1(arrayInputs) {
+	let resultadofind = arrayInputs.includes("")
 
 
 	if (!resultadofind) {
@@ -287,7 +327,7 @@ function comprobar() {
 	if (camposValidos) {
 		ocultarMensajeActividad()
 		//Validamos las 3 primaras cajas.
-		if (arrayvalidador[0] == respuestasJuegoDemo[0] && arrayvalidador[1] == respuestasJuegoDemo[1] && arrayvalidador[2] == respuestasJuegoDemo[2]) {
+		if (arrayInputs[0] == respuestasJuegoDemo[0] && arrayInputs[1] == respuestasJuegoDemo[1] && arrayInputs[2] == respuestasJuegoDemo[2]) {
 			operacion1 = true
 		} else {
 			operacion1 = operacion1
@@ -295,7 +335,7 @@ function comprobar() {
 
 
 		//validamos la ultima caja de la formula.
-		if (arrayvalidador[5] == respuestasJuegoDemo[3]) {
+		if (arrayInputs[5] == respuestasJuegoDemo[3]) {
 			operacion2 = true
 		} else {
 			operacion2 = operacion2
@@ -303,93 +343,196 @@ function comprobar() {
 
 		if (operacion1 && operacion2) {
 			/* document.getElementById('contenedorResultado').style.visibility = "visible" */
-			switch (getCurrentSlider()) {
-				case 1:
-					document.getElementById('resultado-1').classList.add('imagen-correcta')
-					break;
+			document.getElementById('resultado-1').classList.add('imagen-correcta')
 
-				case 2:
-					document.getElementById('resultado-1-actividad').classList.add('imagen-correcta')
-					break;
+			console.log(arrayInputs[5], ((respuestasJuegoDemo[0] + respuestasJuegoDemo[1]) - respuestasJuegoDemo[2]));
 
-				default:
-					break;
-			}
-
-			console.log(arrayvalidador[5] , ((respuestasJuegoDemo[0] + respuestasJuegoDemo[1])-respuestasJuegoDemo[2]));
-
-			if (arrayvalidador[3] == ((respuestasJuegoDemo[0] + respuestasJuegoDemo[1])-respuestasJuegoDemo[2])) {
+			if (arrayInputs[3] == ((respuestasJuegoDemo[0] + respuestasJuegoDemo[1]) - respuestasJuegoDemo[2])) {
 				numerador = true
 			} else {
 				numerador = numerador
 			}
-			
-			if (arrayvalidador[5] == respuestasJuegoDemo[6] ) {
+
+			if (arrayInputs[5] == respuestasJuegoDemo[6]) {
 				denominador = true
 			} else {
 				denominador = denominador
 			}
 			if (numerador == true && denominador == true) {
-				switch (getCurrentSlider()) {
-					case 1:
-						document.getElementById('btnContinuar-demo').classList.remove('enabled')
-						document.getElementById('contenedorResultado').style.visibility = "visible"
-						document.getElementById('resultado-2').classList.add('imagen-correcta')
-						break;
-
-					case 2:
-						document.getElementById('btnContinuar').classList.remove('enabled')
-						document.getElementById('contenedorResultado').style.visibility = "visible"
-						document.getElementById('resultado-2-actividad').classList.add('imagen-correcta')
-						break;
-
-					default:
-						break;
-				}
+				document.getElementById('btnContinuar-demo').classList.remove('enabled')
+				document.getElementById('contenedorResultado').style.visibility = "visible"
+				document.getElementById('resultado-2').classList.add('imagen-correcta')
 
 			}
 			else {
-				switch (getCurrentSlider()) {
-					case 1:
-						document.getElementById('btnContinuar-demo').classList.add('enabled')
-						document.getElementById('contenedorResultado').style.visibility = "visible"
-						document.getElementById('resultado-2').classList.add('imagen-incorrecta')
-						break;
-
-					case 2:
-						document.getElementById('btnContinuar').classList.add('enabled')
-						document.getElementById('contenedorResultado').style.visibility = "visible"
-						document.getElementById('resultado-2-actividad').classList.add('imagen-incorrecta')
-						break;
-
-					default:
-						break;
-				}
+				document.getElementById('btnContinuar-demo').classList.add('enabled')
+				document.getElementById('contenedorResultado').style.visibility = "visible"
+				document.getElementById('resultado-2').classList.add('imagen-incorrecta')
 
 
 			}
 		} else {
-			switch (getCurrentSlider()) {
-				case 1:
-					document.getElementById('resultado-1').classList.add('imagen-incorrecta')
-					break;
-
-				case 2:
-					document.getElementById('resultado-1-actividad').classList.add('imagen-incorrecta')
-					break;
-
-				default:
-					break;
-			}
-
+			document.getElementById('resultado-1').classList.add('imagen-incorrecta')
 		}
 
 	}
 }
 
+function Actividad2(arrayInputs) {
+	let resultadofind = arrayInputs.includes("")
 
-function Actividad1(){
+
+	if (!resultadofind) {
+		camposValidos = true
+
+	} else {
+		mostrarMensajeActividad()
+
+	}
+
+	if (camposValidos) {
+		ocultarMensajeActividad()
+		//validamos Inputs 1-2-3-4
+		//Validamos numeradoees
+		
+		
+		if(arrayInputs[0] == respuestasJuegoActividad1[0].numerador[0]){
+			if(arrayInputs[1] == respuestasJuegoActividad1[0].numerador[1]){
+				if(arrayInputs[2] == respuestasJuegoActividad1[0].numerador[2]){
+					if(arrayInputs[3] == respuestasJuegoActividad1[0].numerador[3]){
+						operacion1 = true
+					}else{
+						operacion1 = operacion1
+					
+					}
+				}
+			}
+		}
 	
+		//Validamos operacion1 denominadores.
+		if (arrayInputs[7] == respuestasJuegoActividad1[1].denominador[0]){
+			if(arrayInputs[8] == respuestasJuegoActividad1[1].denominador[1]){
+				operacion2 = true
+			}else{
+				operacion2 = operacion2
+			}
+		}
+
+
+
+		//validamos la denominadores.
+		if (operacion1 && operacion2) {
+			document.getElementById('resultado-1-actividad1').classList.add('imagen-correcta')
+
+
+			if (arrayInputs[4] == respuestasJuegoActividad1[2].operacion1[0] && arrayInputs[5]==respuestasJuegoActividad1[2].operacion1[1]) {
+				numerador = true
+			} else {
+				numerador = numerador
+			}
+
+			if (arrayInputs[10] == respuestasJuegoActividad1[3].operacion2[0]) {
+				denominador = true
+			} else {
+				denominador = denominador
+			}
+
+
+			if (numerador == true && denominador == true) {
+				document.getElementById('btnContinuar').classList.remove('enabled')
+				document.getElementById('contenedorResultado').style.visibility = "visible"
+				document.getElementById('resultado-2-actividad1').classList.add('imagen-correcta')
+				if(arrayInputs[6]==respuestasJuegoActividad1[4].resultado[0] && arrayInputs[9]==respuestasJuegoActividad1[4].resultado[1] ){
+					document.getElementById('btnContinuar').classList.remove('enabled')
+					document.getElementById('contenedorResultado').style.visibility = "visible"
+					document.getElementById('resultado-3-actividad1').classList.add('imagen-correcta')
+				}else {
+					document.getElementById('btnContinuar').classList.add('enabled')
+					document.getElementById('contenedorResultado').style.visibility = "visible"
+					document.getElementById('resultado-3-actividad1').classList.add('imagen-incorrecta')
+				}
+				
+			}
+			else {
+				document.getElementById('btnContinuar').classList.add('enabled')
+				document.getElementById('contenedorResultado').style.visibility = "visible"
+				document.getElementById('resultado-2-actividad1').classList.add('imagen-incorrecta')
+			}
+
+			
+		} else {
+			document.getElementById('resultado-1-actividad1').classList.add('imagen-incorrecta')
+		}
+
+	}
+}
+
+function Actividad3(arrayInputs) {
+
+	let resultadofind = arrayInputs.includes("")
+
+
+	if (!resultadofind) {
+		camposValidos = true
+
+	} else {
+		mostrarMensajeActividad()
+
+	}
+
+	if (camposValidos) {
+		ocultarMensajeActividad()
+		//Validamos las 3 primaras cajas.
+		if (arrayInputs[0] == respuestasJuegoActividad2[0] && arrayInputs[1] == respuestasJuegoActividad2[1] && arrayInputs[2] == respuestasJuegoActividad2[2]) {
+			operacion1 = true
+		} else {
+			operacion1 = operacion1
+		}
+
+
+		//validamos la ultima caja de la formula.
+		if (arrayInputs[5] == respuestasJuegoActividad2[3]) {
+			operacion2 = true
+		} else {
+			operacion2 = operacion2
+		}
+
+		if (operacion1 && operacion2) {
+			/* document.getElementById('contenedorResultado').style.visibility = "visible" */
+			document.getElementById('resultado-1-actividad2').classList.add('imagen-correcta')
+			
+			console.log(arrayInputs[3], respuestasJuegoActividad2[0] + (respuestasJuegoActividad2[1]) + respuestasJuegoActividad2[2]);
+			console.log(arrayInputs[3], respuestasJuegoActividad2[0], respuestasJuegoActividad2[1],respuestasJuegoActividad2[2]);
+
+			if (arrayInputs[3] == ((respuestasJuegoActividad2[0] + (respuestasJuegoActividad2[1])) + respuestasJuegoActividad2[2])) {
+				numerador = true
+			} else {
+				numerador = numerador
+			}
+
+			if (arrayInputs[5] == respuestasJuegoActividad2[6]) {
+				denominador = true
+			} else {
+				denominador = denominador
+			}
+			if (numerador == true && denominador == true) {
+				document.getElementById('btnContinuar').classList.remove('enabled')
+				document.getElementById('contenedorResultado-actividad2').style.visibility = "visible"
+				document.getElementById('resultado-2-actividad2').classList.add('imagen-correcta')
+
+			}
+			else {
+				document.getElementById('btnContinuar').classList.add('enabled')
+				document.getElementById('contenedorResultado-actividad2').style.visibility = "visible"
+				document.getElementById('resultado-2-actividad2').classList.add('imagen-incorrecta')
+
+
+			}
+		} else {
+			document.getElementById('resultado-1-actividad2').classList.add('imagen-incorrecta')
+		}
+
+	}
 }
 
 function repetirTextosEscena(slide) {
@@ -397,16 +540,17 @@ function repetirTextosEscena(slide) {
 }
 
 function continuar() {
-	document.getElementById('input1').value=""
-	document.getElementById('input2').value=""
-	document.getElementById('input3').value=""
- 	document.getElementById('input4').value=""
-	document.getElementById('input5').value=""
-	document.getElementById('input6').value=""
-	document.getElementById('input7').value=""
-	document.getElementById('input8').value=""
-	document.getElementById('input9').value=""
-	document.getElementById('input10').value=""
+	document.getElementById('input1').value = ""
+	document.getElementById('input2').value = ""
+	document.getElementById('input3').value = ""
+	document.getElementById('input4').value = ""
+	document.getElementById('input5').value = ""
+	document.getElementById('input6').value = ""
+	document.getElementById('input7').value = ""
+	document.getElementById('input8').value = ""
+	document.getElementById('input9').value = ""
+	document.getElementById('input10').value = ""
+	document.getElementById('input11').value = ""
 	document.getElementById('btnContinuar').classList.add('enabled')
 	ReiniciarVariablesActividad()
 }
